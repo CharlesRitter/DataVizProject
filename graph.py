1eb7d9f12a91c90c39ef6a9f3e371a3899f48cdd
crritte <crritte@users.noreply.github.com>
2017-05-19 15:39:25 -0400

    outlined the program in comments

--- graph.py
+++ graph.py
@@ -7,3 +7,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import parse
+
+def visualize_days():
+    """Visualize data by day of the week"""
+
+    #graph our parsed data
+
+    #make a new variable, 'counter', from iterating through each
+    #line of data in the parsed data, and count how many incidents
+    #happen on each day of the week
+
+    #separate the x-axis data (the days of the week) from the
+    #'counter' variable form the y-axis data (the number of)
+    #incidents for each day)
+
+    #with that y-axis data, assign it to a matplotlib instance
+
+    #create the amount of ticks needed fo rour x-axis, and assign
+    #the labels
+
+    #save the plot!
+
+    #close the plot
