af07a63cef0436068d1fc6b7dbb7c2dbe8ff159a
crritte <crritte@users.noreply.github.com>
2017-05-19 16:18:29 -0400

    added counters for the incidences on each day of the week

--- graph.py
+++ graph.py
@@ -12,15 +12,26 @@ def visualize_days():
     """Visualize data by day of the week"""
 
     #grab our parsed data
-
+    data_file = parse(parse.MY_FILE, ",")
 
     #make a new variable, 'counter', from iterating through each
     #line of data in the parsed data, and count how many incidents
     #happen on each day of the week
+    counter = Counter(item["DayOfWeek"] for item in data_file)
 
     #separate the x-axis data (the days of the week) from the
     #'counter' variable form the y-axis data (the number of)
     #incidents for each day)
+    data_list = [
+    counter["Monday"],
+    counter["Tuesday"],
+    counter["Wednesday"],
+    counter["Thursday"],
+    counter["Friday"],
+    counter["Saturday"],
+    counter["Sunday"]
+    ]
+    day_tuple = tuple(["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"])
 
     #with that y-axis data, assign it to a matplotlib instance
 
